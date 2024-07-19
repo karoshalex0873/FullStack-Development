5e4d11b33b7c6c57b5a42ef068f4a3cd461c5c6f
Alex karobia <akarobia30@gmail.com>
2024-07-19 23:47:00 +0300

    Done both the appending and readfile using fs method

--- appendfile.js
+++ appendfile.js
@@ -1,7 +1,13 @@
 var http= require('http');
 var fs= require('fs');
-
-fs.appendFile('myfile.txt','This is appending using node', function(err){
-    if(err) console.log('replace the exiting file?');
-    console.log('saved');
-});
+http.createServer(function(req,res){
+    fs.appendFile('myfile.txt','This is appending using node', function(err){
+        if(err) console.log('replace the exiting file?');
+        console.log('saved');
+    });
+    fs.readFile('myfile.txt',function(err,data){
+        res.writeHead(200,{'Content-type':'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080)
